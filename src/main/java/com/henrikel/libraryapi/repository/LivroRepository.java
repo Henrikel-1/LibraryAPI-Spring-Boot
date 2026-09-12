542ee3c48ca3c98ef6a8212398b944e17e794fe8
Henrikel-1 <keldsonh@gmail.com>
2026-09-12 19:33:37 -0300

    feat: adiciona consultas findByTitulo e existsByTituloIgnoreCase(AndIdNot)

--- src/main/java/com/henrikel/libraryapi/repository/LivroRepository.java
+++ src/main/java/com/henrikel/libraryapi/repository/LivroRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    boolean existsByTituloIgnoreCase(String titulo);
+    boolean existsByTituloIgnoreCaseAndIdNot(String titulo, Long id);
     List<Livro> findByTitulo(String titulo);
 }
