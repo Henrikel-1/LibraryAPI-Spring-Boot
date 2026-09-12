c7d6208e125b54431d97e5fc3723c57e8bd7aed6
Henrikel-1 <keldsonh@gmail.com>
2026-09-12 19:33:32 -0300

    feat: adiciona atributos anoPubli, editora e escritor ao Livro

--- src/main/java/com/henrikel/libraryapi/dto/LivroRequestDTO.java
+++ src/main/java/com/henrikel/libraryapi/dto/LivroRequestDTO.java
@@ -1,10 +1,23 @@
 package com.henrikel.libraryapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
-public record LivroRequestDTO(@NotBlank(message = "Titulo é obrigatório")
+import java.time.Year;
+
+public record LivroRequestDTO(@NotBlank(message = "O Titulo é obrigatório")
                               @Size(min = 1, max = 200)
-                              String titulo) {
+                              String titulo,
+                              @NotNull(message = "O ano de publicação é obrigatório")
+                              @PastOrPresent(message = "O ano de publicação não pode estar no futuro")
+                              Year anoPubli,
+                              @NotBlank(message = "A editora é obrigatório")
+                              @Size(min = 1, max = 200)
+                              String editora,
+                              @NotBlank(message = "O escritor é obrigatório")
+                              @Size(min = 1, max = 200)
+                              String escritor) {
 
 }
--- src/main/java/com/henrikel/libraryapi/model/Livro.java
+++ src/main/java/com/henrikel/libraryapi/model/Livro.java
@@ -5,10 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Year;
 
 @Getter
 @Setter
@@ -19,8 +22,13 @@ public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     private String titulo;
+    @NotNull
+    private Year anoPubli;
+    @NotBlank
+    private String editora;
+    @NotBlank
+    private String escritor;
 
 }
