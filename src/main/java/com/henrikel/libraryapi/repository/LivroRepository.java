4fd06efd9b69c435e5e11eafc0baeecfd95170a3
Henrikel-1 <keldsonh@gmail.com>
2026-09-16 14:56:46 -0300

    refactor: anota LivroRepository com @Repository

--- src/main/java/com/henrikel/libraryapi/repository/LivroRepository.java
+++ src/main/java/com/henrikel/libraryapi/repository/LivroRepository.java
@@ -3,9 +3,10 @@ package com.henrikel.libraryapi.repository;
 
 import com.henrikel.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     boolean existsByTituloIgnoreCase(String titulo);
     boolean existsByTituloIgnoreCaseAndIdNot(String titulo, Long id);
