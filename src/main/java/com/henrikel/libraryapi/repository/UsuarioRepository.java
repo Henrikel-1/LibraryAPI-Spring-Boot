f99b739ee095a4ba4cd9753fd93cf88259081e1d
Henrikel-1 <keldsonh@gmail.com>
2026-09-16 14:56:47 -0300

    feat: adiciona UsuarioRepository com busca por nome e checagem de email existente

---
+++ src/main/java/com/henrikel/libraryapi/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.henrikel.libraryapi.repository;
+
+import com.henrikel.libraryapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
+    List<Usuario> findByNome(String nome);
+}
