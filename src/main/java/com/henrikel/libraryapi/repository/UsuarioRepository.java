ce2f965ae93cdb6143979b724cbaa127f3965038
Henrikel-1 <keldsonh@gmail.com>
2026-09-17 11:19:50 -0300

    feat: adiciona findByEmail ao UsuarioRepository

--- src/main/java/com/henrikel/libraryapi/repository/UsuarioRepository.java
+++ src/main/java/com/henrikel/libraryapi/repository/UsuarioRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
     List<Usuario> findByNome(String nome);
+    Optional<Usuario> findByEmail(String email);
+
 }
